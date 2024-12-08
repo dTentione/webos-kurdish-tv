ec8297370c2e74279f7d5cb4188388b08d5ee742
YM Kurd <114823042+dTentione@users.noreply.github.com>
2024-12-08 20:39:06 +0000

    Add files via upload

---
+++ tools/gen-manifest.cjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const crypto = require('crypto');
+const fs = require('fs');
+
+const outfile = process.argv[2];
+if (!outfile) throw new Error('Usage: gen-manifest <outfile>');
+
+const appinfo = require('../assets/appinfo.json');
+const ipkfile = `${appinfo.id}_${appinfo.version}_all.ipk`;
+const ipkhash = crypto
+  .createHash('sha256')
+  .update(fs.readFileSync(ipkfile))
+  .digest('hex');
+
+fs.writeFileSync(
+  outfile,
+  JSON.stringify({
+    id: appinfo.id,
+    version: appinfo.version,
+    type: appinfo.type,
+    title: appinfo.title,
+    // appDescription: appinfo.appDescription,
+    iconUri:
+      'https://raw.githubusercontent.com/dtentione/webos-kurdish-tv/main/largeIcon.png',
+    sourceUrl: 'https://github.com/dtentione/webos-kurdish-tv',
+    rootRequired: false,
+    ipkUrl: ipkfile,
+    ipkHash: {
+      sha256: ipkhash
+    }
+  })
+);
